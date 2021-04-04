1ba0b372aa7d42f735c4f87af32465f392ad1318
pbandi2020 <65101167+pbandi2020@users.noreply.github.com>
2021-04-04 11:24:25 -0500

    rechecking in the config example

--- Earthquake_Challenge/static/js/config.example.js
+++ Earthquake_Challenge/static/js/config.example.js
@@ -1,3 +1,2 @@
-// API key
-//const API_KEY = "pk.eyJ1IjoicGJhbmRpMjAxNCIsImEiOiJja212YTMxM3UwMzN3Mm9xcG15c285OGxtIn0.9w1ZK69dlYUNWPX0NdbT0Q";
-const API_KEY = "Your Token Here";
+//API key reference
+const API_KEY = "Your Token Key Here"
